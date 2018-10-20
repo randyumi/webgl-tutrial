78e58f95ba7354325312064e33433e7a679e60f3
Kento Ito <k-ito@farmnote.jp>
2018-10-20 15:46:55 +0900

    tochu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "imports-loader?THREE=three!three/examples/js/loaders/OBJLoader.js"
 import * as THREE from "three"
 import MTLLoader from 'three-mtl-loader'
+const OrbitControls = require('three-orbitcontrols')
+const ColladaLoader = require('three-collada-loader')
 
 window.onload = () => {
     const elem = document.getElementById('sample');
@@ -13,8 +15,9 @@ window.onload = () => {
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(455, 1)
+    const orbit = new OrbitControls(camera)
 
-    camera.position.set(0, 0, 2000)
+    camera.position.set(0, 0, 1500)
 
     const box = new THREE.Mesh(
         new THREE.BoxGeometry(200, 200, 200),
@@ -31,33 +34,57 @@ window.onload = () => {
     loader.setPath('./assets/')
     const objLoader = new THREE.OBJLoader();
 
-    const light = new THREE.DirectionalLight(0xFFFFFF)
-    light.position.set(1, 1, 1)
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.3)
+    const light = new THREE.DirectionalLight(0xFFFFFF, 0.5)
+    light.position.set(1, 0, 1)
+    scene.add(ambientLight)
     scene.add(light)
-
-    loader.load('eichi_el_ei_tex.mtl', (materials) => {
-        materials.preload()
-        objLoader.setMaterials(materials)
-        objLoader.setPath('./assets/')
-        objLoader.load('eichi_el_ei_tex.obj', (object) => {
-            object.rotation.set(0, 0, 0)
-            object.position.set(0, 0, 0)
-            object.scale.set(1, 1, 1)
-            const obj = new THREE.Object3D()
-            //obj.add(object)
-            scene.add(object)
-            const tick = () => {
-                object.rotation.y += 0.01;
-                box.rotation.y += 0.01;
-                box2.rotation.x += 0.01
-                box.position.y += 0.5;
-                box2.position.x += 0.5;
-                renderer.render(scene, camera)
-                requestAnimationFrame(tick)
-            }
-            tick()
-        })
+    const daeLoader = new ColladaLoader()
+    daeLoader.load('./assets/eichi_el_ei04.dae', (object) => {
+        console.log(object)
+        const daeScene = object.scene
+        scene.add(daeScene)
+        const mixer = new THREE.AnimationMixer(daeScene)
+        const clock = new THREE.Clock();
+        const animate = () => {
+            renderer.render(scene, camera)
+            orbit.update()
+        }
+        const tick = () => {
+            daeScene.rotation.set(0, 0, 0)
+            daeScene.position.set(0, 0, 0)
+            daeScene.scale.set(1, 1, 1)
+            daeScene.rotation.y += 0.01;
+            renderer.render(scene, camera)
+            requestAnimationFrame(tick)
+        }
+        tick()
     })
 
-
+    // loader.load('eichi_el_ei_tex.mtl', (materials) => {
+    //     materials.preload()
+    //     objLoader.setMaterials(materials)
+    //     objLoader.setPath('./assets/')
+    //     objLoader.load('eichi_el_ei_tex.obj', (object) => {
+    //         console.log(object)
+    //         object.children.shift()
+    //         object.rotation.set(0, 0, 0)
+    //         object.position.set(0, 0, 0)
+    //         object.scale.set(1, 1, 1)
+    //         const obj = new THREE.Object3D()
+    //         //obj.add(object)
+    //         scene.add(object)
+    //         const tick = () => {
+    //             // object.rotation.y += 0.01;
+    //             // box.rotation.y += 0.01;
+    //             // box2.rotation.x += 0.01
+    //             // box.position.y += 0.5;
+    //             // box2.position.x += 0.5;
+    //             object.children[3].rotation.y += 0.01
+    //             renderer.render(scene, camera)
+    //             requestAnimationFrame(tick)
+    //         }
+    //         tick()
+    //     })
+    // })
 }
