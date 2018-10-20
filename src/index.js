edf9998eaf9826c46590382f9976fd3cfa664f7d
randyumi <randyumi0110@gmail.com>
2018-10-20 19:59:58 +0900

    cooperated animattion

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ window.onload = () => {
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(455, 1, 0.1, 5000)
-    camera.position.set(0, 200, 1500)
+    camera.position.set(0, 200, 800)
     const orbit = new OrbitControls(camera)
     const light = new THREE.DirectionalLight(0xFFFFFF, 0.2)
     light.position.set(1, 0, 1)
@@ -35,6 +35,7 @@ window.onload = () => {
 
     const loader = new GltfLoader()
     loader.load('./assets/hli.gltf', (data) => {
+        console.log(data.scene.children)
         data.scene.children = data.scene.children.filter((e) => e.name !== ('tochi'))
         data.scene.scale.set(300, 300, 300)
         scene.add(box)
@@ -43,11 +44,36 @@ window.onload = () => {
         if (data.animations) {
             mixer = new THREE.AnimationMixer(data.scene)
             data.animations.forEach((e) => {
-                mixer.clipAction(e).play()
+                const a = mixer.clipAction(e)
+                a.clampWhenFinished = true
+                switch (e.name) {
+                    case "hAction":
+                        a.startAt(mixer.time + 0.1); break; // eichiのh
+                    case "I_1Action":
+                        a.startAt(mixer.time + 0.2); break;
+                    case "_02cAction":
+                        a.startAt(mixer.time + 0.3); break;
+                    case"I_2Action":
+                        a.startAt(mixer.time + 0); break;
+                    case "aAction":
+                        a.startAt(mixer.time + 0.2); break;
+                    case "lAction":
+                        a.startAt(mixer.time + 0); break; //elのl
+                    case "I_3Action":
+                        a.startAt(mixer.time + 0.1); break; // aiのi
+                    case "_01eAction":
+                        a.startAt(mixer.time + 0.3); break;
+                    case "_03eAction":
+                        a.startAt(mixer.time + 0.3); break;
+                    default:
+                        null;
+                }
+                a.setLoop(THREE.LoopOnce)
+                a.play()
             })
         }
         const tick = () => {
-            scene.rotation.y += 0.001
+            //scene.rotation.y += 0.001
             renderer.render(scene, camera)
             requestAnimationFrame(tick)
         }
